14ba4cb88518c4dc61cb22e6d26a62cbc15e8b61
Prabh Singh <68962901+prabh8331@users.noreply.github.com>
2024-08-07 19:35:16 +0530

    control_flow: loops

--- Section3/day3_rust/src/control_flow.rs
+++ Section3/day3_rust/src/control_flow.rs
@@ -1,2 +1,32 @@
 pub fn loops() {
-    // control flow's also called loops
+
+    // Loops 
+    
+    // loop {
+    //     println!("Simple loop- executes forever unless you explicitly exit out of it");
+    // }
+    
+    // To exit out of loop use break statement
+    loop {
+        println!("Simple loop- with break statment");
+        break;
+    }
+    
+    // if have nested loops, the break will only exit out of the inner loop, but not from outer loop
+    // to tell compiler to exit from outer loop we use labeling.
+    'outer: loop {
+        println!("Simple loop- with labeling");
+        break 'outer;
+    }
+    
+    // loop as an expression with a returning value
+    // in below example 5 will be return when loop breaks
+    // use case of this is - if potentially have a failing operation - in such cases loop will keep attempting the operation until it succeeds then assign the resulting value to varialbe 
+    let a = loop {
+        break 5;
+    };
+    
+    println!("{a}");
+    
+
+}
