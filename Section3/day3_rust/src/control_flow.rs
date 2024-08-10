0896cc8c522a0e680a5b742a9cef3a7976e6cecc
prabh8331 <prabh8331@gmail.com>
2024-08-10 21:33:30 +0530

    control flow - loop, break, for loop, whole loop

--- Section3/day3_rust/src/control_flow.rs
+++ Section3/day3_rust/src/control_flow.rs
@@ -21,12 +21,30 @@ pub fn loops() {
     
     // loop as an expression with a returning value
     // in below example 5 will be return when loop breaks
-    // use case of this is - if potentially have a failing operation - in such cases loop will keep attempting the operation until it succeeds then assign the resulting value to varialbe 
+    // use case of this is - if potentially have a failing operation - 
+    // in such cases loop will keep attempting the operation until it succeeds then assign the resulting value to varialbe 
     let a = loop {
         break 5;
     };
     
     println!("{a}");
+
+
+    // for loop - allows to loop thorugh collections such as vector or arrays etc. 
+
+    let vec = vec![45, 30, 85, 90, 41, 39];
+
+    for i in vec {
+        println!("{i}");
+    }
     
+    // while loops - will continue to execute while a give condition is true
+
+    let mut num = 0;
+
+    while num < 10 {
+        num = num + 1;
+    }
+
 
 }
