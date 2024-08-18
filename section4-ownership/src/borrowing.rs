563932b0bf2fd63fff2fc4c641ad7dc840ebfe18
prabh8331 <prabh8331@gmail.com>
2024-08-19 00:42:47 +0530

    borrowing

--- section4-ownership/src/borrowing.rs
+++ section4-ownership/src/borrowing.rs
@@ -51,7 +51,35 @@ pub fn borrowing() {
         // - At any time, you can have either one mutable reference or any number of immutable references
         // - references must always be valid
     let vec_1 = vec![1, 2, 3];
-    takes_ownership(vec_1);
+    // takes_ownership(vec_1); //  transfer of ownership from Vec1 in the main to the variable vec inside the function and if we try to access this variable after function call it gives error
+
+
+    // takes_ownership(vec_1.clone()); and previously it fixed by cloning vec_1, but a cloning will make a new heap allocation and is therefore not very efficient
+
+    // in this case it is more sensible to pass a reference to vec_1 to function because function is only printing the vector passed and it does not need to take ownership as it is not concerned with the task of determinin when vector shoup be cleaned up
+
+    let ref1 = &vec_1;
+
+    // let ref2 = &mut vec_1; // we can't have this mutable function because the ref2 is comming in the scope of ref1 {which is from above line to bellow line i.e. from created to last used} .. this is because we can't have immutable and mutable reference together
+
+    borrows_vec(ref1); // it is cheaper than cloing because we are not making any new heap allocation as funciton is not taking ownership, so now vec_1 is also available in main
+    println!("vec_1 is : {:?}", vec_1);
+
+
+    // takes_and_gives_ownership(vec_1); here again we are passing ownership of vec_1 inside the funciton so when try to print bellow we gets error
+    // println!("vec_1 is : {:?}", vec_1);
+
+
+    // let vec_1 = takes_and_gives_ownership(vec_1); // we can solve above issue using shadowing because this function returns back the ownership, so returned ownership is assigend to vec_1, but it is not efficient
+    // println!("vec_1 is : {:?}", vec_1);
+
+    // achiving above task without transferring and receiving the ownership using mutable reference
+    // as mutable reference can only be created for mutable variable , so lets make vec_1 mutable 
+    let mut vec_1 = vec![1, 2, 3];
+    let ref2 = &mut vec_1;
+    mutabley_borrows_vec(ref2) ;
+
+    println!("vec_1 is : {:?}", vec_1);
 
 
 
@@ -59,6 +87,31 @@ pub fn borrowing() {
 
 }
 
-fn takes_ownership(vec: Vec<i32>) {
+// fn takes_ownership(vec: Vec<i32>) {  // before passing the reference where we were passing variable itself, and this takes reference
+fn borrows_vec(vec: &Vec<i32>) {  // when passing reference, need to indicate in the function's signature that it expects a reference rather than an owned value, and done using '&' .. renaming from takes_ownership to borrow_vec
     println!("vec is : {:?}", vec);
 }
+
+
+// fn takes_and_gives_ownership(mut vec: Vec<i32>) -> Vec<i32> { // before passing the reference
+fn mutabley_borrows_vec(vec: &mut Vec<i32>) { // updating the sighature so that it accepts a mutable reference and as we don't need to pass back the ownership we remove return 
+    vec.push(10);
+}
+
+
+
+// fn gives_onership() -> &Vec<i32> {
+//     let vec = vec![4, 5, 7];
+//     &vec
+// }
+
+
+// Now if instead of returning the vector, we return a reference to the vector then we will encounter an error missing lifetime specifier.
+
+// The function return type contains a borrowed value, but there is no value for it to be borrowed from.
+
+// In this specific case, the error arises because we are violating borrowing rule number two, which states that references must always remain valid.
+
+// This occurs because inside the function we create the vector vec which has ownership. We then return a reference to the vector, but once the function ends, the vector is dropped and cleaned up, resulting in a dangling reference, meaning that the reference will not be pointing to valid memory.
+
+// Generally , when we create a value within a function and intend to return it, you must transfer ownership of that value. Returning a reference is not advisable because the old value will be automatically cleaned up at the function's end, rendering the reference invalid.
